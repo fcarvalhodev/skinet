0913bec41128af575112490d7d3450b85ec662c9
Fábio Carvalho <fabio.carvalllho@live.com>
2021-10-20 10:24:09 -0300

    starting the pagination configuration

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -25,6 +25,12 @@ namespace Core.Specifications
 
         public Expression<Func<T, object>> OrderByDescending {get; private set;}
 
+        public int Take {get; private set;}
+
+        public int Skip {get; private set;}
+
+        public bool IsPagingEnabled {get; private set;}
+
         protected void AddInclude(Expression<Func<T, object>> includeExpression)
         {
             Includes.Add(includeExpression);
@@ -35,10 +41,17 @@ namespace Core.Specifications
             OrderBy = orderByExpression;
         }
 
-        
         protected void AddOrderByDescending(Expression<Func<T, object>> OrderByDescExpression)
         {
             OrderByDescending = OrderByDescExpression;
         }
+
+        protected void ApplyPaging(int skip, int take) 
+        {
+            Skip = skip;
+            Take = take;
+            IsPagingEnabled = true;
+        }
+        
     }
 }
